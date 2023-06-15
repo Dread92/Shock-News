c93bee242172ac6fd70d531400c0f0e1d9c90b94
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-06-15 17:32:19 +0300

    back to top arrow

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faTwitter, faTelegram } from '@fortawesome/free-brands-svg-icons';
+import { faArrowUp} from '@fortawesome/free-solid-svg-icons';
 import './Footer.scss';
 
 export const Footer = () => {
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  const handleScrollToTop = () => {
+    setIsScrolling(true);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    setTimeout(() => {
+      setIsScrolling(false);
+    }, 1000); // Adjust the timeout value based on your desired scroll duration
+  };
+
   return (
     <footer className="footer">
-      
       <div className="social-icons">
         <a href="https://www.facebook.com/your-facebook-page" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faFacebook} />
@@ -22,6 +32,9 @@ export const Footer = () => {
         </a>
       </div>
       <span>© ShockNews 2023</span>
+      <button className={`back-to-top ${isScrolling ? 'scrolling' : ''}`} onClick={handleScrollToTop}>
+        <FontAwesomeIcon icon={faArrowUp} />
+      </button>
     </footer>
   );
 };
