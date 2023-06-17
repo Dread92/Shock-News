db843c785825b47132d1fcb878da1938959ce48f
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-06-17 05:03:09 +0300

    hydra logo and blinking effect.

--- src/components/TorchlightEffect.jsx
+++ src/components/TorchlightEffect.jsx
@@ -6,11 +6,13 @@ function TorchlightEffect() {
   const wrapperRef = useRef(null);
   const heroRef = useRef(null);
   const overlayRef = useRef(null);
+  const hydraRef = useRef(null);
 
   useEffect(() => {
     const wrapper = wrapperRef.current;
     const hero = heroRef.current;
     const overlay = overlayRef.current;
+    const hydra = hydraRef.current;
 
     /* Timeline */
     const tl = gsap.timeline();
@@ -56,36 +58,41 @@ function TorchlightEffect() {
     const wrapper = wrapperRef.current;
     const hero = heroRef.current;
     const overlay = overlayRef.current;
-    const heading = hero.querySelector('.hero__heading');
 
     wrapper.classList.add('fade-out');
     gsap.to(hero, {
       opacity: 0,
       duration: 0.5,
       onComplete: () => {
-        // Create and animate the overlay
         gsap.set(overlay, { visibility: 'visible', backgroundColor: 'black', opacity: 1 });
         gsap.to(overlay, {
           duration: 0.5,
           backgroundColor: 'transparent',
           onComplete: () => {
-            // Hide the overlay after the animation completes
             gsap.set(overlay, { visibility: 'hidden' });
-
-            // Animate the content to appear smoothly
             gsap.fromTo(
               wrapper.children,
               { opacity: 0, y: 20 },
               { opacity: 1, y: 0, duration: 0.5, stagger: 0.2 }
             );
-
-            // Unmount the component after the animation completes
             wrapper.style.display = 'none';
           },
         });
       },
     });
   };
+
+  useEffect(() => {
+    const hydra = hydraRef.current;
+
+    const blinkRandomly = () => {
+      const randomDelay = Math.random() * 800 + 500; // Random delay between 500ms and 2500ms
+      hydra.style.setProperty('--blink-delay', `${randomDelay}ms`);
+      setTimeout(blinkRandomly, randomDelay);
+    };
+
+    blinkRandomly();
+  }, []);
 
   return (
     <div className="wrapper" ref={wrapperRef} onClick={handleClick}>
@@ -95,6 +102,9 @@ function TorchlightEffect() {
 
       <div className="hero hero--secondary" aria-hidden="true" ref={heroRef} data-hero>
         <p className="hero__heading">Click me and follow me into the rabbit hole...</p>
+        <div className="hydra" ref={hydraRef}>
+          <img className="hydraimg" src="hydre.png" alt="Hydra" />
+        </div>
       </div>
 
       <div className="overlay" ref={overlayRef}></div>
@@ -103,3 +113,4 @@ function TorchlightEffect() {
 }
 
 export default TorchlightEffect;
+
